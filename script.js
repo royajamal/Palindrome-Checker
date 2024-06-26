2dce1df80e729c5656e61b22102a61a55345befd
unknown <royajamal2003@gmail.com>
2024-06-26 03:16:41 -0700

    added new changes in script.js file

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const checkButton = document.getElementById('check-btn');
-    const resultDiv = document.getElementById('result');
+const resultDiv = document.getElementById('result');
 
-    checkButton.addEventListener('click', function() {
-      const userInput = document.getElementById('text-input').value;
+checkButton.addEventListener('click', function() {
+  const userInput = document.getElementById('text-input').value;
 
-      if (userInput.trim().length === 0) {
-        alert('Please input a value');
-      } else {
-        const cleanedInput = userInput.toLowerCase().replace(/[^a-z0-9]/g, '');
-        const reversedInput = cleanedInput.split('').reverse().join('');
+  if (userInput.trim().length === 0) {
+    alert('Please input a value');
+  } else {
+    const cleanedInput = userInput.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedInput = cleanedInput.split('').reverse().join('');
         
-        if (cleanedInput === reversedInput) {
-          resultDiv.innerText = userInput + ' is a palindrome';
-        } else {
-          resultDiv.innerText = userInput + ' is not a palindrome';
-        }
-      }
-    });
+    if (cleanedInput === reversedInput) {
+      resultDiv.innerText = userInput + ' is a palindrome';
+    } else {
+      resultDiv.innerText = userInput + ' is not a palindrome';
+    }
+  }
+});

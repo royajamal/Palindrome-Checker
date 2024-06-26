9ca32db6c54a5e9066afcf0b4e4559a85b52b7ec
unknown <royajamal2003@gmail.com>
2024-06-26 03:22:31 -0700

    added chenges in script.js file

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const checkButton = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
 
-checkButton.addEventListener('click', function() {
+checkButton.addEventListener('click',  () {
   const userInput = document.getElementById('text-input').value;
 
   if (userInput.trim().length === 0) {
-    alert('Please input a value');
+    ('Please input a value');
   } else {
     const cleanedInput = userInput.toLowerCase().replace(/[^a-z0-9]/g, '');
     const reversedInput = cleanedInput.split('').reverse().join('');
-        
     if (cleanedInput === reversedInput) {
       resultDiv.innerText = userInput + ' is a palindrome';
-    } else {
+    }else{
       resultDiv.innerText = userInput + ' is not a palindrome';
     }
   }

54ba0a8d19a2599b4501191116be91bf4884a4a1
unknown <royajamal2003@gmail.com>
2024-06-27 05:07:07 -0700

    script.js linters error solved

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 const checkButton = document.getElementById('check-btn');
-    const resultDiv = document.getElementById('result');
+const resultDiv = document.getElementById('result');
 
-    checkButton.addEventListener('click', () => {
-      const userInput = document.getElementById('text-input').value;
+checkButton.addEventListener('click', () => {
+  const userInput = document.getElementById('text-input').value;
 
-      if (userInput.trim().length === 0) {
-        alert('Please input a value');
-      } else {
-        const cleanedInput = userInput.toLowerCase().replace(/[^a-z0-9]/g, '');
-        const reversedInput = cleanedInput.split('').reverse().join('');
-        
-        if (cleanedInput === reversedInput) {
-          
-         result.textContent = userInput + ' is a palindrome';
-        } else {
-         result.textContent  = userInput + ' is not a palindrome';
-        }
-      }
-    });
+  if (userInput.trim().length === 0) {
+    alert('Please input a value');
+  } else {
+    const cleanedInput = userInput.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedInput = cleanedInput.split('').reverse().join('');
+        if (cleanedInput === reversedInput) { 
+      result.textContent = `${userInput} +  is a palindrome`;
+    } else {
+      result.textContent = `${userInput} +  is not a palindrome`;
+    }
+  }
+});
